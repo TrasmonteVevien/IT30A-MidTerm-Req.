f95da358e53dc20b2de9c0b28d1863873e4ec409
vevien <vevienabiera@gmail.com>
2024-11-04 21:53:31 +0800

    update something

--- register.php
+++ register.php
@@ -78,7 +78,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         button {
             width: 100%;
             padding: 12px;
-            background-color: #4CAF50;
+            background-color: #007bff;
             color: white;
             border: none;
             border-radius: 4px;
